f8d96dbbf20f22669347e7b670d7ff8a45a9b7d4
MuhammadRifqi748 <ssoyuz748@gmail.com>
2025-02-20 14:31:40 +0700

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+
+
+
+<template>
+    <footer class="bg-gray-800 text-white text-center p-4 mt-4">
+      <p>&copy; 2025 Pokédex App. All rights reserved.</p>
+    </footer>
+  </template>
+  
+  
+  
+  
+  <style>
+html, body, #app {
+    height: 100%;
+  }
+  
+  .flex-grow {
+    flex-grow: 1;
+  }
+  
+</style>
+  
